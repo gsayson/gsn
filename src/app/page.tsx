3343175da3455c2461005c7f4657500c4440de68
gsayson <geruls@broskiclan.org>
2023-02-18 11:07:49 +0800

    add buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import "./globals.css"
 import Image from "next/image";
 import ParticlesBackground from "@/components/particles";
+import { IoLogoGithub, IoLogoDiscord } from "react-icons/io5"
 
 export default function Home() {
     return (
@@ -24,7 +25,27 @@ export default function Home() {
                     <h1>Welcome.</h1>
                     <p>I&apos;m <span style={{
                         color: "rgb(45,255,196)"
-                    }}>Gerard Sayson</span>, a computer scientist and aspiring mathematician.</p>
+                    }}>Gerard Sayson</span>, a computer scientist and aspiring mathematician based in <span style={{
+                        color: "rgb(45,255,196)"
+                    }}>Singapore</span>. People call me by a lot of names online, namely <span style={{
+                            color: "rgb(45,255,196)"
+                        }}>Big Rat</span> and <span style={{
+                            color: "rgb(45,255,196)"
+                        }}>Rin</span>.</p>
+                    <br/>
+                    <p>Currently, I&apos;m <em>very</em> interested in compiler design.</p>
+                    <br/><br/>
+                    <div className="center" style={{
+                        flexDirection: "row",
+                        width: "100%"
+                    }}>
+                        <button onClick={
+                            () => {
+                                window.location.assign("https://github.com/gsayson")
+                            }
+                        }><IoLogoGithub/>&nbsp;My GitHub</button>
+                        <button><IoLogoDiscord/>&nbsp;My Discord</button>
+                    </div>
                     <br/>
                 </div>
             </div>
